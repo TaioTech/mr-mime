7ad715b7b9581b6d72adaedbd8aed01e13827869
Tyler Carolan <39821811+TaioTech@users.noreply.github.com>
2025-07-12 18:55:56 -0500

    Implement simple todo list

--- mr-mime/src/__tests__/App.test.tsx
+++ mr-mime/src/__tests__/App.test.tsx
@@ -1,9 +1,26 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../renderer/App';
 
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
+  });
+
+  it('allows adding, editing and deleting items', () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText('Add item');
+    fireEvent.change(input, { target: { value: 'task' } });
+    fireEvent.click(screen.getByLabelText('add-item'));
+    expect(screen.getByText('task')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByLabelText('edit-item'));
+    const editInput = screen.getByLabelText('edit-input');
+    fireEvent.change(editInput, { target: { value: 'updated' } });
+    fireEvent.click(screen.getByLabelText('save-item'));
+    expect(screen.getByText('updated')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByLabelText('delete-item'));
+    expect(screen.queryByText('updated')).toBeNull();
   });
 });
